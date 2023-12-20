e0170d4a2cf4e77284678b256aa58c23e037fe0a
vyportfolio1 <vyportfolio1@gmail.com>
2023-12-20 15:43:29 +0200

    gier

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const bikes = [
         id: 'bike1',
         name: "Quantum Glide Pro",
         imageUrl: "img/bike1.jpg",
-        buttonText: "Read More"
+        buttonText: "Read More",
+        link: "#"
     },
     {
         id: 'bike2',
         name: "Nebula Striker Elite",
         imageUrl: "img/bike2.jpg",
-        buttonText: "Read More"
+        buttonText: "Read More",
+        link: "#"
     },
     {
         id: 'bike3',
         name: "Stealth Thunderbolt X5",
         imageUrl: "img/bike3.jpg",
-        buttonText: "Read More"
+        buttonText: "Read More",
+        link: "#"
     }
 ];
 
@@ -29,8 +32,113 @@ bikes.forEach(bike => {
     bikeElement.innerHTML = `
         <p>${bike.name}</p>
         <img src="${bike.imageUrl}" alt="bike">
-        <button>${bike.buttonText}</button>
+        <a href="${bike.link}"><button>${bike.buttonText}</button></a>
     `;
 
     bikesContainer.appendChild(bikeElement);
 });
+
+
+
+
+
+
+// accessories iteams
+ 
+const accessories = {
+    main: {
+        class: 'access',
+        products: [
+            {
+                class: 'prod01',
+                title: 'Clothing & Armour',
+                icon: 'fa-solid fa-plus',
+                image: '/img/accesor01.png',
+                alt: 'accessories'
+            }
+        ]
+    },
+    smallProducts: {
+        class: 'prod-small',
+        products: [
+            {
+                class: 'prod02',
+                title: 'Helmet',
+                icon: 'fa-solid fa-plus',
+                image: '/img/hel v02.png',
+                alt: 'accessories'
+            },
+            {
+                class: 'prod02',
+                title: 'Lock',
+                icon: 'fa-solid fa-plus',
+                image: '/img/lock 01.png',
+                alt: 'accessories'
+            },
+            {
+                class: 'prod02 hid',
+                title: 'Helmet',
+                icon: 'fa-solid fa-plus',
+                image: '/img/hel v01.png',
+                alt: 'accessories'
+            },
+            {
+                class: 'prod02 hid',
+                title: 'Lock',
+                icon: 'fa-solid fa-plus',
+                image: '/img/lock 02.png',
+                alt: 'accessories'
+            }
+        ]
+    }
+};
+
+
+function appendAccessoriesToDOM() {
+
+    const mainContainer = document.querySelector('.access'); 
+
+
+    const mainProductsHTML = accessories.main.products.map(product => `
+        <div class="${product.class}">
+            <p>${product.title}</p>
+            <i class="${product.icon}"></i>
+            <img src="${product.image}" alt="${product.alt}">
+        </div>
+    `).join('');
+
+
+    const smallProductsHTML = accessories.smallProducts.products.map(product => `
+        <div class="${product.class}">
+            <p>${product.title}</p>
+            <i class="${product.icon}"></i>
+            <img src="${product.image}" alt="${product.alt}">
+        </div>
+    `).join('');
+
+ 
+    mainContainer.innerHTML = `
+        <div class="${accessories.main.class}">
+            ${mainProductsHTML}
+        </div>
+        <div class="${accessories.smallProducts.class}">
+            ${smallProductsHTML}
+        </div>
+    `;
+}
+
+
+appendAccessoriesToDOM();
+
+
+const bars = document.querySelector(".fa-bars-staggered")
+const meniu = document.querySelector(".burger-meniu")
+const close = document.querySelector(".fa-rectangle-xmark")
+
+bars.addEventListener("click", ()=>{
+    meniu.classList.remove("visible")
+})
+
+close.addEventListener("click", ()=>{
+    meniu.classList.add("visible")
+})
